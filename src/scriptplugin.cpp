c6e44511cf6781ca1eb53b177c121896956a61c5
Dominik Seichter <domseichter@web.de>
2008-01-27 18:52:27 +0000

    ADDED: missing files

---
+++ src/scriptplugin.cpp
@@ -0,0 +1,106 @@
+/***************************************************************************
+                   scriptplugin.cpp  -  description
+                             -------------------
+    begin                : Fri Nov 9 2007
+    copyright            : (C) 2007 by Dominik Seichter
+    email                : domseichter@web.de
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+
+#include "scriptplugin.h"
+
+#include <kapplication.h>
+#include <kiconloader.h>
+#include <klistwidget.h>
+#include <klocale.h>
+
+#include <QLabel>
+#include <QHBoxLayout>
+
+#include <kjs/completion.h>
+#include <kjs/interpreter.h>
+#include <kjs/value.h>
+
+ScriptPlugin::ScriptPlugin()
+{
+    m_name = "ScriptPlugin";
+    m_interpreter = new KJS::Interpreter();
+
+    QString script = "alert(\"Hello\"); 4 + 6;";
+
+    KJS::Completion comp = m_interpreter->evaluate( KJS::UString(), 0, KJS::UString( script.toUtf8().data() ) );
+    if( comp.complType() == KJS::ReturnValue )
+    {
+        qDebug( comp.value()->toString( m_interpreter->globalExec() ).cstring().c_str() ); 
+    }
+    else
+        qDebug("compType=%i\n", comp.complType() ); 
+}
+
+ScriptPlugin::~ScriptPlugin()
+{
+
+}
+
+QString ScriptPlugin::processFile( BatchRenamer* b, int index, const QString & filenameOrToken, EPluginType eCurrentType )
+{
+
+    return QString::null;
+}
+
+bool ScriptPlugin::supports( const QString & token )
+{
+    QString lower = token.toLower();
+
+    for( int i = 0; i < m_keys.count(); i++ )
+        // TODO: Maybe we can optimize by putting all tokens
+        //       already converted to lowercase into m_keys
+        if( QRegExp( m_keys[i].toLower() ).exactMatch( lower ) )
+            return true;
+            
+    return false;
+}
+
+const QPixmap ScriptPlugin::icon() const
+{
+    return KIconLoader::global()->loadIcon( m_icon, K3Icon::Small );
+}
+
+void ScriptPlugin::createUI( QWidget* parent ) const
+{
+    QSpacerItem* spacer = new QSpacerItem( 20, 20, QSizePolicy::Expanding, QSizePolicy::Expanding );
+    
+    QVBoxLayout* l    = new QVBoxLayout( parent );
+    QHBoxLayout* hbox = new QHBoxLayout( parent );
+    
+    QLabel* pix = new QLabel( parent );
+    pix->setPixmap( KIconLoader::global()->loadIcon( m_icon, K3Icon::Desktop ) );
+    
+    hbox->addWidget( pix );
+    hbox->addWidget( new QLabel( "<qt><b>"+name()+"</b></qt>", parent  ) );
+    hbox->addItem( spacer );
+
+    l->addLayout( hbox );    
+    l->addWidget( new QLabel( i18n("This plugin can execute custom scripts."), parent  ) );
+    l->addWidget( new QLabel( i18n("Supported tokens:"), parent  ) );
+
+    KListWidget* list = new KListWidget( parent  );
+    //list->setColumnMode( KListBox::FitToWidth );
+    
+    const QStringList & keys = supportedTokens();
+
+    for( unsigned int i = 0; i < keys.count(); i++ )
+        list->insertItem( 0, "[" + keys[i] + "]" );
+    
+    l->addWidget( list );
+    l->setStretchFactor( list, 2 );
+}
+
---
+++ src/scriptplugin.h
@@ -0,0 +1,170 @@
+/***************************************************************************
+                        scriptplugin.h  -  description
+                             -------------------
+    begin                : Fri Nov 9 2007
+    copyright            : (C) 2007 by Dominik Seichter
+    email                : domseichter@web.de
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+
+#ifndef _SCRIPT_PLUGIN_H_
+#define _SCRIPT_PLUGIN_H_
+
+#include "plugin.h"
+
+namespace KJS {
+    class Interpreter;
+};
+
+class ScriptPlugin : public Plugin {
+ public:
+    /** Create a new ScriptPlugin from
+     *
+     */
+    ScriptPlugin();
+
+    virtual ~ScriptPlugin();
+
+    /** 
+     * @returns a name of the plugin that can be displayed
+     *          to the user. This name should be internationalized.
+     */
+    inline virtual const QString name() const;
+
+    /** 
+     * @returns the type of the plugin.
+     */
+    inline virtual int type() const;
+
+    /**
+     * @returns an icon for this plugin.
+     */
+    virtual const QPixmap icon() const;
+
+    /**
+     * @returns true if this plugins is always enabled
+     *
+     * Warning: If you return true here, the user has no possibility to
+     *          disable this plugin.
+     */
+    inline virtual bool alwaysEnabled() const;
+
+    /**
+     * This function is the core of your plugin.
+     *
+     * It does the actual processing of a file, filename or token depending of the type 
+     * of your plugin.
+     *
+     * \see type()
+     *
+     * @param b the parent BatchRenamer instance calling this plugin
+     * @param index the index of the current file (i.e. the first file has index 0,
+     *              the second file to be renamed has index 1 ....)
+     * @param filenameOrToken this parameter depends on the type of your plugin.
+     *                        If type is ePluginType_File, this is the absolute path
+     *                        or URL to the renamed file.
+     *                        If type is ePluginType_Filename, this is the filename
+     *                        (without path) as created by KRename.
+     *                        If type is ePluginType_Token, this is the contents of a token
+     *                        in brackets. If your plugin supports the token [example],
+     *                        KRename will pass the strign "example" to your method.
+     * @param eCurrentType the current type of plugin that is requested (for plugins that support more than one type)
+     *
+     * @returns the result of the function, depending on type().
+     * @returns QString::null if this plugin has nothing to do.
+     * @returns A new filename if type is ePluginType_Filename
+     * @returns the value of the token if type is ePluginType_Token
+     * @returns an error message or QString::null if type is ePluginType_File
+     */
+    virtual QString processFile( BatchRenamer* b, int index, const QString & filenameOrToken, EPluginType eCurrentType );
+
+    /** Get a list of all tokens supported by this plugin. 
+     *
+     *  If the token type != ePluginType_Token you have to return an empty list
+     *
+     *  @returns a list of all supported tokens.
+     */
+    inline virtual const QStringList & supportedTokens() const;
+
+    /** Returns help descriptions for the supported tokens
+     *
+     *  The returned stringlist contains strings that are the tokens
+     *  and the description separated by ;;
+     *
+     *  @returns a stringlist containing help on the supported tokens
+     */
+    inline virtual const QStringList & help() const;
+
+    /** Create a user interface for this plugin
+     *
+     *  @param parent the parent widget of this plugin
+     *
+     *  This is implemented here for all FilePlugin based classed
+     */
+    void createUI( QWidget* parent ) const;
+
+ protected:
+    /** 
+     *  Checks if a token is supported by this plugin.
+     *
+     *  @param token a token
+     *  @returns true if the token is supported
+     *
+     *  @see addSupportedToken
+     */
+    bool supports( const QString & token );
+    
+    /**
+     *  Adds a token to the list of supported tokens
+     *
+     *  @param token will be a supported token from now on
+     *
+     *  @see supports
+     */
+    inline void addSupportedToken( const QString & token ) { m_keys.append( token ); }
+ 
+ protected:
+
+    QString m_name;
+    QString m_icon;
+
+ private:
+    QStringList       m_keys;
+    KJS::Interpreter* m_interpreter;
+};
+
+
+inline const QString ScriptPlugin::name() const
+{
+    return m_name;
+}
+
+inline bool ScriptPlugin::alwaysEnabled() const
+{
+    return true;
+}
+
+inline int ScriptPlugin::type() const
+{
+    return ePluginType_Token;
+}
+
+inline const QStringList & ScriptPlugin::supportedTokens() const 
+{ 
+    return m_keys; 
+}
+
+inline const QStringList & ScriptPlugin::help() const 
+{ 
+    return m_keys; 
+}
+
+#endif // _FILE_PLUGIN_H_
